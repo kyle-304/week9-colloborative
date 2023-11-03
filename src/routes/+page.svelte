e15c56efbb3c1204e51fe9948a5c7dbc0f59e810
kyle sting <kyleonchoke2001@gmai.com>
2023-11-03 16:31:03 +0300

    third commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+	import { goto } from "$app/navigation";
+
+</script>
+
 <h1 class="text-xs">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="/home">go to home</a>
